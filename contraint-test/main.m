55e3253ba51c0d9a9e9dd50b7324f734378a56ac
luowei <745054025@qq.com>
2014-12-11 18:36:19 +0800

    Initial Commit

---
+++ contraint-test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  contraint-test
+//
+//  Created by luowei on 14/12/11.
+//  Copyright (c) 2014年 luowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
